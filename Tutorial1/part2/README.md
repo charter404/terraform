fadc6ff8cc66bede73265e40f35d6b15b4d6cd6f
Charter404 <charter404@hotmail.com>
2025-10-22 20:46:49 +0530

    part 2

---
+++ Tutorial1/part2/README.md
@@ -0,0 +1,3 @@
+This part will show the basic of terraform
+How to add provider initializing all the steps in the terraform
+For AWS and GCP both
---
+++ Tutorial1/part2/gcp_provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      versin = "4.47.0"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = "4.47.0"
+    }
+  }
+}
